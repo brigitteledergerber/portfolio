f22c1e9630c44204ddfa15a4fd8604ee8008b82d
brigittelederge <brigitte.ledergerber1@gmail.com>
2019-11-19 17:20:49 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Brigitte Ledergerber Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="About Me">Ledergerber_AboutMe.html</a></li></br>
+      <li><a href="Projects">Ledergerber_Projects.html</a></li></br>
+      <li><a href="Resume">Ledergerber_Resume.html</a></li></br>
+      <li><a href="Contact Me">Ledergerber_ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
